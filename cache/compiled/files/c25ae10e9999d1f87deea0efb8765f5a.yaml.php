48cefc4e7d8e36c34bd315c4a1ba6a771b3ab0c5
mohsin1993 <mohsanriaz1993@gmail.com>
2018-10-12 13:30:11 +0200

    fix

--- cache/compiled/files/c25ae10e9999d1f87deea0efb8765f5a.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/Users/mohsanriaz/Sites/grav-admin/user/config/streams.yaml',
-    'modified' => 1537701574,
-    'data' => [
-        
-    ]
-];
