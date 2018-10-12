9b1e89630e321d038eab35538acdbb0541c6a845
mohsin1993 <mohsanriaz1993@gmail.com>
2018-10-12 13:33:31 +0200

    fix

---
+++ cache/compiled/files/5a04a06324636853da29809a79822310.yaml.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/mohsanriaz/Sites/grav-admin/user/plugins/minify-html/minify-html.yaml',
+    'modified' => 1538600368,
+    'data' => [
+        'enabled' => true,
+        'cache' => false,
+        'mode' => 'default'
+    ]
+];
---
+++ cache/compiled/files/c25ae10e9999d1f87deea0efb8765f5a.yaml.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/mohsanriaz/Sites/grav-admin/user/config/streams.yaml',
+    'modified' => 1537701574,
+    'data' => [
+        
+    ]
+];
