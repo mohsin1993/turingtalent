d2eb27f05030778c88449251825ed5efe8dc7943
mohsin1993 <mohsanriaz1993@gmail.com>
2018-10-16 02:57:11 +0200

    link fix

---
+++ cache/compiled/files/8c336cf31c7f8e5eea0cdcff7cdd94b7.yaml.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/mohsanriaz/Sites/grav-admin/user/plugins/error/error.yaml',
+    'modified' => 1539546041,
+    'data' => [
+        'enabled' => true,
+        'routes' => [
+            404 => '/error'
+        ]
+    ]
+];
---
+++ cache/compiled/files/d11145057a1370ad8e9da546b4644fa5.yaml.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/mohsanriaz/Sites/grav-admin/system/blueprints/config/media.yaml',
+    'modified' => 1539546039,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.MEDIA',
+        'form' => [
+            'validation' => 'loose',
+            'fields' => NULL
+        ]
+    ]
+];
